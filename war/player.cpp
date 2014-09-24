892553595b1f3f32d241c5aa5991f5a48bdf3935
MooseNosebleed <alfordzw1@gcc.edu>
2014-09-23 23:45:06 -0400

    Still without shuffle

--- war/player.cpp
+++ war/player.cpp
@@ -9,7 +9,28 @@ Player::Player()
 	//discard.resize(0);	Only if we have such a deck
 }
 
-Player::player(vector<Card> deckA)
+Player::Player(vector<Card> deckA)
 {
-	
+	deck=deckA;
+	discard.resize(0);
+}
+
+void::Player addCard(Card A)
+{
+	deck.push_back(A);
+	//discard.push_back(A);		We should pick one of these.
+}
+
+Card::Player playCard();
+{
+	Card temp;
+	temp=deck[0];
+	deck.erase(deck.begin());
+	return temp;
+}
+
+int::Player numCards();
+{
+	int temp;
+	temp=deck.size();//+discard.size();		Same as above
 }
